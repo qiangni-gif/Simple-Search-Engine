0e21028b2602c7a5782fb17be5943c0e63b15984
bloodteller123 <chenzihan708@gmail.com>
2020-01-29 16:44:41 -0500

    corpus access

---
+++ src/corpus_access_module.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+import os
+import json
+
+
+# In[52]:
+
+
+corpus = '../output/storage.json'
+excerptLength = 50
+
+
+# In[56]:
+
+
+def getDocs(docIds):
+    docIds.sort()
+    if os.path.exists(corpus):
+        output = []
+        with open(corpus, 'r') as file:
+            f = json.load(file)
+            l = [d for d in f if d['docId'] in docIds]
+        for course in l:
+            s = course['desc']
+            d = dict()
+            if len(s) > excerptLength:
+                s = s[:excerptLength]+'...'
+                d['link'] = course['docId']
+                d['title'] = course['title']
+            d['desc'] = s
+            output.append(d)
+    return output
+
+
+# In[57]:
+
+
+#docIds = [1,3,2,4]
+
+
+# In[58]:
+
+
+#getDocs(docIds)
+
