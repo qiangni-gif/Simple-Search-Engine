b0fc48903777d6575001448f03b1a573643fca24
qiangni <34262202+qiangni@users.noreply.github.com>
2020-02-02 17:44:40 -0500

    Update user_interface_module.py
    update

--- src/user_interface_module.py
+++ src/user_interface_module.py
@@ -5,6 +5,7 @@
 import sys
 import dictionary_building_module as db
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 
 
 class Ui_mainWindow(object):
@@ -25,6 +26,7 @@ class Ui_mainWindow(object):
         self.scrollArea.setLocale(QtCore.QLocale(QtCore.QLocale.Armenian, QtCore.QLocale.Armenia))
         self.scrollArea.setWidgetResizable(True)
         self.scrollArea.setObjectName("scrollArea")
+
         self.scrollAreaWidgetContents = QtWidgets.QWidget()
         self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 814, 604))
         self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
@@ -32,14 +34,9 @@ class Ui_mainWindow(object):
         self.gridLayout.setContentsMargins(5, 5, 5, 5)
         self.gridLayout.setSpacing(10)
         self.gridLayout.setObjectName("gridLayout")
-        self.scrollArea_2 = QtWidgets.QScrollArea(self.scrollAreaWidgetContents)
-        self.scrollArea_2.setWidgetResizable(True)
-        self.scrollArea_2.setObjectName("scrollArea_2")
-        self.scrollAreaWidgetContents_2 = QtWidgets.QWidget()
-        self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(0, 0, 802, 442))
-        self.scrollAreaWidgetContents_2.setObjectName("scrollAreaWidgetContents_2")
-        self.scrollArea_2.setWidget(self.scrollAreaWidgetContents_2)
-        self.gridLayout.addWidget(self.scrollArea_2, 5, 0, 1, 5)
+        self.listWidget = QtWidgets.QListWidget(self.scrollAreaWidgetContents)
+        self.listWidget.setObjectName("listWidget")
+        self.gridLayout.addWidget(self.listWidget, 5, 0, 1, 5)
 
         self.checkBox_3 = QtWidgets.QCheckBox(self.scrollAreaWidgetContents)
         self.checkBox_3.setEnabled(True)
@@ -52,13 +49,15 @@ class Ui_mainWindow(object):
         self.checkBox_3.setLocale(QtCore.QLocale(QtCore.QLocale.Armenian, QtCore.QLocale.Armenia))
         self.checkBox_3.setChecked(True)
         self.checkBox_3.setObjectName("checkBox_3")
+        self.gridLayout.addWidget(self.checkBox_3, 3, 3, 1, 1)
         self.checkBox_3.stateChanged.connect(self.onCheckBox_Toggled)
 
-        self.gridLayout.addWidget(self.checkBox_3, 4, 3, 1, 1)
+
         self.lineEdit = QtWidgets.QLineEdit(self.scrollAreaWidgetContents)
         self.lineEdit.setLocale(QtCore.QLocale(QtCore.QLocale.Armenian, QtCore.QLocale.Armenia))
         self.lineEdit.setObjectName("lineEdit")
         self.gridLayout.addWidget(self.lineEdit, 0, 2, 1, 3)
+
         self.label = QtWidgets.QLabel(self.scrollAreaWidgetContents)
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
@@ -69,6 +68,18 @@ class Ui_mainWindow(object):
         self.label.setLocale(QtCore.QLocale(QtCore.QLocale.Armenian, QtCore.QLocale.Armenia))
         self.label.setObjectName("label")
         self.gridLayout.addWidget(self.label, 1, 1, 1, 1)
+
+        self.label_2 = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_2.setFont(font)
+        self.label_2.setLocale(QtCore.QLocale(QtCore.QLocale.Armenian, QtCore.QLocale.Armenia))
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 2, 1, 1, 1)
+
         self.pushButton_2 = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
@@ -78,12 +89,22 @@ class Ui_mainWindow(object):
         self.pushButton_2.setFont(font)
         self.pushButton_2.setObjectName("pushButton_2")
         self.gridLayout.addWidget(self.pushButton_2, 4, 0, 1, 1)
+        self.pushButton_2.clicked.connect(self.onViewDetail_clicked)
+
         self.comboBox = QtWidgets.QComboBox(self.scrollAreaWidgetContents)
         self.comboBox.setLocale(QtCore.QLocale(QtCore.QLocale.Armenian, QtCore.QLocale.Armenia))
         self.comboBox.setObjectName("comboBox")
         self.comboBox.addItem("")
         self.comboBox.addItem("")
         self.gridLayout.addWidget(self.comboBox, 1, 2, 1, 1)
+
+        self.comboBox_2 = QtWidgets.QComboBox(self.scrollAreaWidgetContents)
+        self.comboBox_2.setLocale(QtCore.QLocale(QtCore.QLocale.Armenian, QtCore.QLocale.Armenia))
+        self.comboBox_2.setObjectName("comboBox_2")
+        self.comboBox_2.addItem("")
+        self.comboBox_2.addItem("")
+        self.gridLayout.addWidget(self.comboBox_2, 2, 2, 1, 1)
+
         self.checkBox_2 = QtWidgets.QCheckBox(self.scrollAreaWidgetContents)
         self.checkBox_2.setEnabled(True)
         font = QtGui.QFont()
@@ -95,9 +116,9 @@ class Ui_mainWindow(object):
         self.checkBox_2.setLocale(QtCore.QLocale(QtCore.QLocale.Armenian, QtCore.QLocale.Armenia))
         self.checkBox_2.setChecked(True)
         self.checkBox_2.setObjectName("checkBox_2")
+        self.gridLayout.addWidget(self.checkBox_2, 2, 3, 1, 1)
         self.checkBox_2.stateChanged.connect(self.onCheckBox_Toggled)
 
-        self.gridLayout.addWidget(self.checkBox_2, 3, 3, 1, 1)
         self.checkBox = QtWidgets.QCheckBox(self.scrollAreaWidgetContents)
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
@@ -120,6 +141,8 @@ class Ui_mainWindow(object):
         self.pushButton.setFont(font)
         self.pushButton.setLocale(QtCore.QLocale(QtCore.QLocale.Armenian, QtCore.QLocale.Armenia))
         self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.onSearch_clicked)
+
         self.gridLayout.addWidget(self.pushButton, 0, 0, 1, 2)
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
         self.gridLayout_2.addWidget(self.scrollArea, 1, 0, 1, 1)
@@ -136,9 +159,11 @@ class Ui_mainWindow(object):
         mainWindow.setWindowTitle(_translate("mainWindow", "Simple Search Engine"))
         self.checkBox_3.setText(_translate("mainWindow", "Normalization"))
         self.label.setText(_translate("mainWindow", "Type of model"))
+        self.label_2.setText(_translate("mainWindow", "Type of collection"))
         self.pushButton_2.setText(_translate("mainWindow", "View Detail"))
         self.comboBox.setItemText(0, _translate("mainWindow", "Boolean Retrieval Model"))
         self.comboBox.setItemText(1, _translate("mainWindow", "Vector Space Model"))
+        self.comboBox_2.setItemText(0, _translate("mainWindow", "UofO catalog"))
         self.checkBox_2.setText(_translate("mainWindow", "Stemming"))
         self.checkBox.setText(_translate("mainWindow", "Stopword "))
         self.pushButton.setText(_translate("mainWindow", "Search"))
@@ -161,13 +186,50 @@ class Ui_mainWindow(object):
         #print("stopWordFlag is %s" %db.stopWordFlag)
         #print("wordStemmingFlag is %s" %db.wordStemmingFlag)
         #print("normalizationFlag is %s" %db.normalizationFlag)
+    
+    def onSearch_clicked(self):
+        model = self.comboBox.currentText()
+        collection = self.comboBox_2.currentText()
+        print("onSearch_clicked, module: "+model+" collection: "+ collection)
+
+        if self.lineEdit.text() == "":
+            msg = QMessageBox()
+            msg.setWindowTitle("warning!")
+            msg.setText("Please fill out the search field!")
+            font = QtGui.QFont()
+            font.setFamily("Times New Roman")
+            font.setPointSize(10)
+            msg.setFont(font)
+            
+            x = msg.exec_()
+        else:
+            query = self.lineEdit.text()
+            self.listWidget.addItem(query)
+
+    
+    def onViewDetail_clicked(self):
+        print("onViewDetail_clicked")
+        item = self.listWidget.currentItem()
+
+        info = QMessageBox()
+        info.setWindowTitle("Doc")
+        info.setText(item.text())
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(10)
+        info.setFont(font)
+            
+        y = info.exec_()
+
+
+    
 def main():
     app = QtWidgets.QApplication(sys.argv)
     w = QtWidgets.QMainWindow()
     ui = Ui_mainWindow()
     ui.setupUi(w)
     #print("corpus preprocessing...")
-    db.pre_dictionary_building()
+    #db.pre_dictionary_building()
     w.show()
     sys.exit(app.exec_())
 
