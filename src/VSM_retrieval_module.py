456a8e5e329f293cf9ff87d928cdf486d5b952f8
qiangni <34262202+qiangni@users.noreply.github.com>
2020-02-13 16:41:17 -0500

    Update VSM_retrieval_module.py

--- src/VSM_retrieval_module.py
+++ src/VSM_retrieval_module.py
@@ -8,6 +8,7 @@ import weight_calculation_module as wc
 #https://stackoverflow.com/questions/26924812/python-sort-list-of-json-by-value
 
 weightedindexPath = '../output/weightedindex.json'
+countMax = 30
 
 def gettf_idf():
     wc.getweightedindex()
@@ -19,30 +20,41 @@ def extractQueryTerms(query):
     query = db.stopWordRemoval(query)
     query = db.wordStemming(query)
     query = db.normalization(query)
-    print(query)
+    return query
+
+def count(query):
+    w = {}
     for x in query:
         if x not in w:
             y = query.count(x)
             w[x] = y
     return w
-
 #search query -> list of ranked docId with score
 def comput_score(query):
+    c = 0
+    wquery = count(query)
     score = {}
     with open(weightedindexPath,'r') as f:
         windex = json.load(f)
-        wquery = extractQueryTerms(query)
-        print(wquery)
         for q,w in wquery.items():
             for i,t in windex.items():
                 for x in t:
-                    if q == i:
+                    if q == i and c <= countMax:
+                        c = c + 1
                         #print(x)
                         if x[0] not in score:
                             score[x[0]] = x[1]*w
                         else:
                             score[x[0]] += x[1]*w
         #sort the list by score
-        rank = sorted(score.items(), key=lambda item:item[1], reverse=True)
+        if score != {}:
+            rank = sorted(score.items(), key=lambda item:item[1], reverse=True)
+            score = {}
+            c = 0
+        else:
+            rank = None
+            score = {}
+            c = 0
         #print(rank)
     return rank
+
